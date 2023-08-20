01b1ad0e80be8b34c483a51f2d335cd3bc3c25fa
MAhsanNaveed <132484012+MAhsanNaveed@users.noreply.github.com>
2023-08-20 23:14:12 +0500

    Update filterMethod.js

--- filterMethod.js
+++ filterMethod.js
@@ -30,3 +30,81 @@ function specificLenght(inputArray,userLength){
     return filteredArray;
 }
 console.log(specificLenght(['apple','banana','kiwi','watermellon','mangoes','grapes'],6));// output ['banana', 'watermellon', 'mangoes', 'grapes']
+
+// _________ Q.No#04
+
+//------   filter the objects on the bases of property 
+const students=[
+    {
+        name:"Ahsan",
+        score:80,
+    },
+    {
+        name:"Naveed",
+        score:78,
+    },
+    {
+        name:"Waqas",
+        score:90,
+    },
+    {
+        name:'Imran',
+        score:95,
+    }
+]
+/// filter the all students which have specific score given to the function
+
+function filterData(inputObject, inputScore){
+    
+    const filteredObject= inputObject.filter(element => element.score >= inputScore);
+    
+    return filteredObject;
+}
+console.log(filterData(students,90));
+
+// output [{name: 'Waqas', score: 90},{name: 'Imran', score: 95}]
+
+// _________ Q.No#05
+
+//------   filter out the null or undefined value from the array
+
+const mixedValues = [1, null, "apple", undefined, true, false];
+
+//  remove the null and undefined
+
+function filterValues(inputArray){
+    filteredArray= inputArray.filter(element => {
+        if (element !== null && element !== undefined){
+            // if each value is not null or undefined than it will move the value in filtered array
+            return element;
+        }
+    });
+    return filteredArray;
+};
+
+console.log(filterValues(mixedValues));// output [1, 'apple', true]
+
+
+
+// _________ Q.No#06
+
+//------   filter out the prime number in the array 
+
+let arr=[1,2,3,4,5,6,7,8,9,10]; 
+
+let primeNum=arr.filter(isPrime);
+
+console.log(primeNum);
+
+function isPrime(number){
+    if(number <=1){
+        return false;
+    }
+    for(let i=2;i<=Math.sqrt(number);i++){
+        if(number%i===0){
+            return false;
+        }
+    }
+    return true;
+}
+
