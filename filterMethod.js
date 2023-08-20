8419b3b6b754df5723620b06d5c25b66f2b1dd71
MAhsanNaveed <132484012+MAhsanNaveed@users.noreply.github.com>
2023-08-20 13:02:39 +0500

    Update filterMethod.js

--- filterMethod.js
+++ filterMethod.js
@@ -1,7 +1,11 @@
 /// filter out the duplicate elements in an array
 
  let numbers=[0,1,0,3,4,3,2,1,5,6,7,1,5,4,6,7,9];
+
  let uniquNumbers=numbers.filter((value,index,inputArray)=>{
-     return inputArray.indexOf(value)===index;
+  
+     return inputArray.indexOf(value)===index;///  it will find the index of first occurence if the element is present at that place 
+  // it will be filtered after the first location of element it will never be true so the element will be skipped
+ 
  })
  console.log(uniquNumbers);
