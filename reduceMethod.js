742fe89529e354315549a06f883ed22de551f2cd
MAhsanNaveed <132484012+MAhsanNaveed@users.noreply.github.com>
2023-08-26 17:52:52 +0500

    Update reduceMethod.js

--- reduceMethod.js
+++ reduceMethod.js
@@ -40,3 +40,42 @@ function uniqueElements(inputArray){
   },[]);
 }
 console.log(uniqueElements(arr));
+
+// Q6. Write a function to reverse the order of elements in an array using reduce().
+let arr=[1,2,3,4,2,4,2,4,3,3,5,6,4,5,1,5,6]
+
+ function reverseOrder(inputArray){
+   return inputArray.reduceRight((acc,value)=>{
+     acc.push(value);
+     return acc;
+   },[]);
+}
+ console.log(reverseOrder(arr));
+
+// Q7. Write a function to multiply all elements of an array together using reduce().
+ let arr=[1,2,3,4,2]
+ function productOfArray(input){
+   return input.reduce((acc,value)=>acc*=value,1);
+ }
+ console.log(productOfArray(arr));
+
+// Q8. Write a function to find the average value of all elements in an array using reduce().6-++-
+ let arr=[1,2,3,4,2,4,2,4,3,3,5,6,4,5,1,5,6]
+ function averageOfArray(input){
+   return (input.reduce((acc,value)=>acc+=value,0))/arr.length;
+ }
+ console.log(averageOfArray(arr));
+// Q9. Write a function to find the product of the even elements in an array using reduce().
+ let arr=[1,2,3,4,2,4,7,9]
+ function productOfEven(input){
+   return input.reduce((acc,value)=>value%2===0?acc*=value:acc,1)
+ }
+ console.log(productOfEven(arr));
+// Q10. Write a function to find the index of the largest element in an array using reduce().
+let arr = [1,2,3,4,5,6,11,7,8,9,10];
+function indexOfLargetNumber(inputArray){
+  let largestValue=inputArray.reduce((acc,value)=> value >acc?acc=value:acc,-Infinity);
+  return inputArray.indexOf(largestValue);
+}
+console.log(indexOfLargetNumber(arr));
+
